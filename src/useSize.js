666130452c86eccd53f4cda47b23c4564d5313e0
ryuken73 <ryuken73@gmail.com>
2024-09-04 00:13:43 +0900

    update: add new resize observer

---
+++ src/useSize.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+import useResizeObserver from '@react-hook/resize-observer'
+
+const useSize = (target) => {
+  const [size, setSize] = React.useState()
+
+  React.useLayoutEffect(() => {
+    if(target.current === null) return;
+    setSize(target.current.getBoundingClientRect())
+  }, [target])
+
+  // Where the magic happens
+  useResizeObserver(target, (entry) => setSize(entry.contentRect))
+  return size
+}
+
+export default useSize;

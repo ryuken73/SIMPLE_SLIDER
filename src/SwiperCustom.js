4096644f35d8ab6cda1fe574800f6c617c5ddd01
ryuken73 <ryuken73@gmail.com>
2024-09-09 23:07:06 +0900

    refactor: SwiperCustom.js remove unused

--- src/SwiperCustom.js
+++ src/SwiperCustom.js
@@ -24,14 +24,11 @@ const SwiperWrapper = styled.div`
 function SwiperCustom(props) {
   const {speed, children} = props;
   const [totalCount, setTotalCount] = React.useState(null);
-  // const [swiperSize, setSwiperSize] = React.useState({});
-  // console.log('###', swiperSize)
   const WrappedComponents = React.useCallback(() => {
     let slideIndex = 0;
     const result = [];
     React.Children.forEach(children, (child) => {
       if(child.type.name === 'SliderCustom'){
-        // result.push(React.cloneElement(child, {index: slideIndex, setSwiperSize, speed}));
         result.push(React.cloneElement(child, {index: slideIndex, speed}));
         slideIndex++
       } else {
